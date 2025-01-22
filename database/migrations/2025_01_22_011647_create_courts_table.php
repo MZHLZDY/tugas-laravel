2b039b0ea08670eadb469ad21f5b6bf973143b94
Mirzha Lazuardy Arsy <mirzhalazuardi@gmail.com>
2025-01-22 09:00:01 +0700

    praktikum migrate laravel

--- database/migrations/2025_01_22_011647_create_courts_table.php
+++ database/migrations/2025_01_22_011647_create_courts_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('courts', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->unsignedBigInteger('court_type_id');
+            $table->foreign('court_type_id')->references('id')->on('court_types');           
+            $table->text('name');
+            $table->integer('price');
         });
     }
 
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('courts');
     }
 };
